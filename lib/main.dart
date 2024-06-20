24f975fe822b448e256819fbab4c7243ef720d93
CelerinaD <21-14289@g.batstate-u.edu.ph>
2024-06-20 12:11:09 +0800

    Act 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,73 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('KCTech App'),
-        ),
-        backgroundColor: Colors.brown,
-      ),
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    backgroundColor: Colors.grey,
+    appBar: AppBar(
+      title: Text('User Profile'),
+      backgroundColor: Colors.brown,
+      centerTitle: true,
+    ),
+    body: Padding(
+      padding:  EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'NAME:',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.black,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            'Celerina C. De Mesa',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
 
-      body: Center(
-        child: Image.network('https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?cs=srgb&dl=pexels-pixabay-38238.jpg&fm=jpg'),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'YEAR:',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.black,
+              letterSpacing: 2.0,
+
+            ),
+          ),
+          Text(
+            'Fourth Year',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'EMAIL:',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.black,
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            '21-14289@g.batstate-u.edu.ph',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(
-            Icons.add,
-            color: Colors.blueAccent,
-           ),
-         ),
-        ),
-      ));
-}
+    ),
+
+  ),
+));
