4d18290c4df31f0092548a6be06e9a88f6d9995c
CelerinaD <21-14289@g.batstate-u.edu.ph>
2024-06-26 11:45:39 +0800

    dashboard and Menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_appfront/home.dart';
+import 'package:mobile_appfront/pages/dashboard.dart';
+import 'package:mobile_appfront/pages/menu.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+ routes: {
+   '/' : (context) => Dashboard(),
+   '/menu' : (context) => Menu(),
+   '/profile' : (context) => Home(),
+ },
 ));
 
 
