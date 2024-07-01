ef12a8c263e0d2bc2d7d853939073b92f3b3f397
CelerinaD <21-14289@g.batstate-u.edu.pd>
2024-07-01 11:34:18 +0800

    Activity 5

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:mobile_appfront/home.dart';
 import 'package:mobile_appfront/pages/dashboard.dart';
 import 'package:mobile_appfront/pages/menu.dart';
+import 'package:mobile_appfront/signup.dart';
 
 void main() => runApp(MaterialApp(
- initialRoute: '/menu',
+ initialRoute: '/signup',
   routes: {
    '/' : (context) => Dashboard(),
    '/menu' : (context) => Menu(),
    '/profile' : (context) => Home(),
+   '/signup' : (context) => Signup(),
  },
 ));
 
